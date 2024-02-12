399c123d38bcd278732bffdc1fada574ca11ef1f
geetha0523 <geetha0523@gmail.com>
2024-02-11 20:34:20 -0600

    sms commit

---
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	Student saveStudent(Student student);
+	Student getStudentId(Long id);
+	Student updateStudent(Student student);
+	void deleteStudentById(Long id);
+
+}
